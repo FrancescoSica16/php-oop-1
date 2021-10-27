36d32701ba09cd73e028b87a030b06c84ed041de
FrancescoSica16 <francesco.sica16@gmail.com>
2021-10-27 17:27:27 +0200

    aggiunta di 2 funzione

--- index.php
+++ index.php
@@ -7,21 +7,29 @@ public $regista;
 public $dataUscita;
 public $attori;
 
-/*
-* @param
-*/
-public function __construct($_nome , $_regista, $_dataUscita , $_attori){
+    public function __construct($_nome , $_regista, $_dataUscita , $_attori){
     $this -> nome = $_nome;
     $this -> regista = $_regista;
     $this -> dataUscita = $_dataUscita;
     $this -> attori = $_attori;
+    }
 
-    // perchè trattino basso?
+    public function setRegista($regista) {
+         $this -> regista = $regista;
+    }   
+
+    public function getAttori(){
+        return $this -> attori;
+    }
 }
 
+$inception =  new Movie( "Inception", "paolo", "10/10/2021", "di caprio" );
+$Interstellar =  new Movie( "Interstellar","luca", "21/10/2021", "alfred" );
 
+$inception -> setRegista("regista da funzione");
 
-}
+var_dump($inception, $Interstellar);
 
-
+$attore_interstellar =  $Interstellar ->getAttori();
+var_dump($attore_interstellar);
 ?>
