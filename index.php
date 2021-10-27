876815187534da45aff422103234f9bd971c30c6
FrancescoSica16 <francesco.sica16@gmail.com>
2021-10-27 16:15:41 +0200

    agiunta cotruttore

--- index.php
+++ index.php
@@ -7,6 +7,20 @@ public $regista;
 public $dataUscita;
 public $attori;
 
+/*
+* @param
+*/
+public function __construct($_nome , $_regista, $_dataUscita , $_attori){
+    $this -> nome = $_nome;
+    $this -> regista = $_regista;
+    $this -> dataUscita = $_dataUscita;
+    $this -> attori = $_attori;
+
+    // perchè trattino basso?
+}
+
+
+
 }
 
 
